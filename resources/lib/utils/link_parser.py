ede2e966cf1659f36f1b364eaf1bd1e2d817c567
Billy Nguyen <billy.nguyen@noria.no>
2019-01-11 13:52:39 +0700

    Update release

--- resources/lib/utils/link_parser.py
+++ resources/lib/utils/link_parser.py
@@ -10,21 +10,20 @@ class LinkParser:
         self.url = url
 
     def get_link(self):
-        print(self.url)
         if re.search('ok.ru', self.url):
             return self.get_link_ok()
 
         return self.url
 
     def get_link_ok(self):
-        response = Request().get(self.url)
-
-        m = re.search('data-options=\"([^\"]+)\"', response)
-        h = HTMLParser.HTMLParser()
-        content = h.unescape(m.group(1))
-        m = re.search('\"metadata\":\"(.*})\",', content).group(1)
-        m = re.search('\"videos\":(\[.*}\]),', m).group(1)
-        print(m)
+        # response = Request().get(self.url)
+        #
+        # m = re.search('data-options=\"([^\"]+)\"', response)
+        # h = HTMLParser.HTMLParser()
+        # content = h.unescape(m.group(1))
+        # m = re.search('\"metadata\":\"(.*})\",', content).group(1)
+        # m = re.search('\"videos\":(\[.*}\]),', m).group(1)
+        # print(m)
 
         # m = re.search(";(?P<hlsurl>[^;]+video\.m3u8.+?)\\&quot;", response)
         # print(m.group(1).replace(u'\\\\u0026', u'&'))
