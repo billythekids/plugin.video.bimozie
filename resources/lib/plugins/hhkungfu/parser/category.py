6e0a3979abe123119d021bdb8108fb22436171cc
PhatDatPQ <61587405+PhatDatPQ@users.noreply.github.com>
2022-04-11 21:11:52 +0700

    fix hhkungfu.tv
    fix hhkungfu.tv

--- resources/lib/plugins/hhkungfu/parser/category.py
+++ resources/lib/plugins/hhkungfu/parser/category.py
@@ -1,8 +1,21 @@
-# -*- coding: utf-8 -*-
+# coding=utf-8
+from bs4 import BeautifulSoup
+from kodi_six.utils import py2_encode
 
 
 class Parser:
     def get(self, response):
         category = []
+        soup = BeautifulSoup(response, "html.parser")
 
+        cats = soup.select('ul#menu-menu > li')
+
+        for item in cats:
+            menu = item.select_one('a')
+            if menu.get("href") == '/': continue # skip 1st
+            category.append({
+                'title': py2_encode(menu.text.strip()),
+                'link': menu.get("href")
+                # 'subcategory': Parser.getsubmenu(item)
+            })
         return category
