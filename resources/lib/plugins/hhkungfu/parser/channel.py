58735d4a3ef73609c73382f57f2be2274bb52a60
PhatDatPQ <61587405+PhatDatPQ@users.noreply.github.com>
2022-04-11 21:11:05 +0700

    fix hhkungfu.tv
    fix hhkungfu.tv

--- resources/lib/plugins/hhkungfu/parser/channel.py
+++ resources/lib/plugins/hhkungfu/parser/channel.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
-
-import math
-
 from bs4 import BeautifulSoup
-from six.moves.html_parser import HTMLParser
-from utils.xbmc_helper import text_encode
-
-from .. import per_page
+import re
+from kodi_six.utils import py2_encode
+import utils.xbmc_helper as helper
 
 
 class Parser:
-    # Display movies in category list
     def get(self, response, page):
 
         channel = {
@@ -19,38 +14,88 @@ class Parser:
             'movies': []
         }
 
-        doc = BeautifulSoup(response)
-
+        soup = BeautifulSoup(response, "html.parser")
         # get total page
-        total_items = doc.find('opensearch:totalresults').text
-        channel['page'] = math.ceil(int(total_items) / per_page)
+        last_page = soup.select('ul.page-numbers > li > a.page-numbers')
+        helper.log("*********************** Get pages ")
+        if len(last_page) > 1:
+            channel['page'] = int(last_page[-2].text.strip())
 
-        h = HTMLParser()
-        i = 1 + ((page - 1) * per_page)
-        for item in doc.findAll('entry'):
-            m_type = ""
-            intro = label = real_title = title = item.title.text
+        for movie in soup.select('a.halim-thumb'):
+            title = movie.get('title').strip()
+            realtitle = movie.select_one('h2.entry-title').text.strip()
+            thumb = movie.select_one('img.img-responsive').get('src')
+            type = movie.select_one('span.episode').text.strip()      
+            label = "[%s] %s" % (type, title)
+            intro = ""
 
-            soup = BeautifulSoup(h.unescape(item.content.text), "html.parser")
-            image_tag = soup.select_one('img')
-            thumbnail = image_tag.get('src')
-
-            intro_tag = soup.select_one('p.noidung')
-            if intro_tag:
-                intro = intro_tag.text
-
-            link = item.find(attrs={"rel": "alternate"}).get('href')
             channel['movies'].append({
-                'id': link,
-                'label': text_encode(label),
-                'title': text_encode(title),
-                'realtitle': text_encode(real_title),
-                'intro': text_encode(intro),
-                'thumb': thumbnail,
-                'type': text_encode(m_type)
+                'id': movie.get('href'),
+                'label': py2_encode(label),
+                'title': py2_encode(title),
+                'realtitle': py2_encode(realtitle),
+                'thumb': thumb,
+                'type': py2_encode(type),
+                'intro': py2_encode(intro),
             })
 
-            i += 1
-            channel['page_patten'] = '?start-index={}&max-results={}'.format(i, per_page)
+        return channel
 
+    def getTop(self, response):
+
+        channel = {
+            'page': 1,
+            'page_patten': None,
+            'movies': []
+        }
+
+        soup = BeautifulSoup(response, "html.parser")
+        for movie in soup.select('#halim-advanced-widget-3-ajax-box > article > div > a.halim-thumb'):
+            # tag = movie.select_one('a')
+            title = movie.get('title').strip()
+            realtitle = type = ""
+            if movie.select_one('span.episode') is not None:
+                type = movie.select_one('span.episode').text.strip()           
+            if movie.select_one('h2.entry-title') is not None:
+                realtitle = movie.select_one('h2.entry-title').text.strip()
+            if realtitle is not None:
+                label = "[%s] %s - %s" % (type, title, realtitle)
+            else:
+                label = "[%s] %s" % (type, title)
+            thumb = movie.select_one('img.img-responsive').get('src')           
+            channel['movies'].append({
+                'id': py2_encode(movie.get('href')),
+                'label': py2_encode(label),
+                'intro': py2_encode(label),
+                'title': py2_encode(title),
+                'realtitle': py2_encode(realtitle),
+                'thumb': thumb,
+                'type': py2_encode(type),
+            })
+        return channel
+
+    def search_result(self, response):
+        channel = {
+            'page': 1,
+            'page_patten': None,
+            'movies': []
+        }
+
+        try:
+            soup = BeautifulSoup(response, "html.parser")
+
+            for movie in soup.select('a.halim-thumb'):
+                title = movie.get('title').strip()
+                link = movie.get('href')
+                thumb = movie.select_one('img.img-responsive').get('src')
+                channel['movies'].append({
+                    'id': movie.get('href'),
+                    'label': title,
+                    'title': title,
+                    'realtitle': title,
+                    'intro': title,
+                    'thumb': thumb,
+                    'type': ''
+                })
+        except: pass
         return channel
